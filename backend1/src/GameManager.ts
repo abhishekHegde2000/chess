f83bbf9e1709b4958b4e93b59f7d769289688d57
Abhishek Hegde <ahegde2021@gmail.com>
2024-04-22 18:47:01 +0530

    step 1 - done. moves using in meory. first system design part completed

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,8 +1,61 @@
-interface Game {
-    id: number;
-    name: string;
-    player1: WebSocket;
-    player2: WebSocket;
-}
+import { WebSocket } from "ws";
+import { INIT_GAME, MOVE } from "./messages";
+import { Game } from "./Game";
 
-export class GameManager {}
+// User, Game
+
+export class GameManager {
+    private games: Game[];
+    private pendingUser: WebSocket | null;
+    private users: WebSocket[];
+
+    constructor() {
+        this.games = [];
+        this.pendingUser = null;
+        this.users = [];
+    }
+
+    addUser(socket: WebSocket) {
+        this.users.push(socket);
+        this.addHandler(socket);
+    }
+
+    removeUser(socket: WebSocket) {
+        this.users = this.users.filter((user) => user !== socket);
+        // Stop the game here because the user left
+    }
+
+    private addHandler(socket: WebSocket) {
+        socket.on("message", (data) => {
+            console.log("inside addHandler");
+
+            const message = JSON.parse(data.toString());
+
+            if (message.type === INIT_GAME) {
+                if (this.pendingUser) {
+                    const game = new Game(this.pendingUser, socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
+                } else {
+                    this.pendingUser = socket;
+                }
+            }
+
+            if (message.type === MOVE) {
+                console.log("inside move");
+                const game = this.games.find(
+                    (game) => game.player1 === socket || game.player2 === socket
+                );
+                if (game) {
+                    console.log("inside makemove");
+                    console.log("\n \n message =  \n \n", message);
+                    console.log(
+                        "\n \n message.payload =  \n \n",
+                        message.payload
+                    );
+                    game.makeMove(socket, message.payload.move);
+                }
+            }
+        });
+    }
+}
